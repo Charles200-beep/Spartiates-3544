a21d7fcada2d5b15663d602dbaf867c4e4479db3
Charles200-beep <charlespetit911@gmail.com>
2020-02-02 13:33:26 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
 
 //Définir les composantes
 public class Robot extends TimedRobot {
@@ -22,6 +24,11 @@ public class Robot extends TimedRobot {
   private final WPI_VictorSPX m_rightMotor = new WPI_VictorSPX(8);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   private final Joystick m_stick = new Joystick(0);
+  
+ AHRS ahrs = new AHRS(SPI.Port.kMXP); 
+
+
+
 
 //Accéder aux données du limelight
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -29,18 +36,21 @@ public class Robot extends TimedRobot {
   NetworkTableEntry ty = table.getEntry("ty");
   NetworkTableEntry ta = table.getEntry("ta");
 
-  //Lire les données du limelight
-double x = tx.getDouble(0.0);
-double y = ty.getDouble(0.0);
-double area = ta.getDouble(0.0);
-
-
- 
 //Conduire avec 'arcade drive'
   @Override
   public void teleopPeriodic() {
     m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
 
+  //Lire les données du limelight
+double x = tx.getDouble(0.0);
+double y = ty.getDouble(0.0);
+double area = ta.getDouble(0.0);
+
+//Lire les données du navX
+double anglemesure = ahrs.getYaw();
+double vitesseangulaire = ahrs.getRawGyroX();
+SmartDashboard.putNumber("anglemesure", anglemesure);
+SmartDashboard.putNumber("vitesseangulaire", vitesseangulaire);
 //Poster au smart dashboard les données du limelight
 SmartDashboard.putNumber("LimelightX", x);
 SmartDashboard.putNumber("LimelightY", y);
@@ -48,28 +58,32 @@ SmartDashboard.putNumber("LimelightArea", area);
 
 //Système de controle automatique
 if (m_stick.getRawButton(1)) {
-if (x>-3.0 & x<3.0) {
-    m_robotDrive.arcadeDrive(m_stick.getY(), 0.0);
+    m_robotDrive.arcadeDrive(-0.7, -x*0.04);
+  
+  }
+//Tourner a un angle
+if (m_stick.getRawButton(2)) {
+  ahrs.reset();
+
 } else {
-    m_robotDrive.arcadeDrive(m_stick.getY(), -x*0.037);
   
-  }
-  
+}
     
-  }
-  {
-    
-  }
+if (m_stick.getRawButton(3)) {
+  double erreur = 90.0 - anglemesure;
+  m_robotDrive.arcadeDrive(0.0, -erreur*90);
+
+} else {
   
-  {
+}
 
+
+
+}//Fin du teleop.periodic
 
 
 }
 
-
-}
-}
 
 
 
